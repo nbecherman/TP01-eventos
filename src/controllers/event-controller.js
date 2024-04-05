7ebbbfd8fa17253ebdf1bd1175562b61d78960f5
NICOLÁS DAVID BECHERMAN <47700068@ort>
2024-04-05 12:06:49 -0300

    5/4

--- src/controllers/event-controller.js
+++ src/controllers/event-controller.js
@@ -1,23 +1,89 @@
-import express from "express";
-import { eventService } from "../servicios/event-service.js";
+import express, { request } from "express";
+import { eventService, eventService1,eventService2,eventService3 } from "../servicios/event-service.js";
 
 const EventService = new eventService();
+const EventService1 = new eventService1();
+const EventService2 = new eventService2();
+const EventService3 = new eventService3();
+
+
 
 const router = express.Router();
 
 router.get("/", (request, response) => {
-    //const limit = request.query.limit;
-    //const offset = request.query.offset;
-    const limit = 20;
-    const offset= 1;
-    try {
-      console.log("get")
-      const allEvents = EventService.getAllEvents(limit, offset);
-      return response.json(allEvents);
+  //const limit = request.query.limit;
+  //const offset = request.query.offset;
+  const limit = 20;
+  const offset = 1;
+  try {
+    const allEvents = EventService.getAllEvents(limit, offset);
+    return response.json(allEvents);
+  } catch (error) {
+    return response.json("Un Error");
+  }
+});
+
+//3
+
+router.get("/", (request, response) => {
+  const pageSize = request.query.pageSize; //limit
+  const page = request.query.page; //offset
+  const nombre = request.query.nombre;
+  const categoria = request.query.categoria;
+  const fechaI = request.query.fechaI;
+  const tag = request.query.tag;
+
+  if (!isNaN(Date.parse(fechaI))) { //si no es date 
+  try {
+      const filter = EventService1.getEventByFilter(
+        1,
+        1,
+        "Gualeguaychu",
+        "Guaracha",
+        "2021/07/01",
+        "tag"
+      );
+      return response.json(filter);
     } catch (error) {
       console.log("Un Error en el controller");
       return response.json("Un Error");
     }
-  });
+  } else {
+    return response.json("La fecha no es datetime");
+  }
+});
 
-  export default router;
+//4
+router.get("/", (request, response) => {
+  const pageSize = request.query.pageSize; //limit
+  const page = request.query.page; //offset
+  const nombre = request.query.id; //pedir id desde el url
+
+  try {
+      const filter = EventService1.getEventByFilter(
+        1,
+      );
+      return response.json(filter);
+    } catch (error) {
+      console.log("Un Error en el controller");
+      return response.json("Un Error");
+    }
+  }
+);
+
+
+
+router.get("/:id",(request,response) =>{
+const id = request.query.id;
+try {
+  const allEvents = EventService2.getEventDetail(1);
+  return response.json(allEvents);
+} catch (error) {
+  return response.json("Un Error");
+}
+});
+
+
+
+
+export default router;
--- src/servicios/event-service.js
+++ src/servicios/event-service.js
@@ -1,23 +1,43 @@
-export class eventService{
-    
-        getAllEvents(){
-        
-        console.log("getAllEvents")
-            return(
-                [
-                    {
-                        id: 1,
-                        name: "evento1",
-                        price: 10,
+export class eventService {
+  getAllEvents() {
+    console.log("getAllEvents");
+    return [
+      {
+        id: 1,
+        name: "evento1",
+        price: 10,
+      },
+    ];
+  }
+}
 
-                    }
-                ]
-            )
-        }
-    }
+export class eventService1 {
+  getEventByFilter(pageSize, page, nombre, categoria, fechaI, tag) {
+    /*Deberá permitir buscar por nombre, categoria, fecha de inicio o un tag determinado. 
+      Para especificar el término de búsqueda o filtros se deberán enviar como parámetros de query.
+      Recuerde que puedo filtrar por uno solo o cualquier combinación de los parámetros.*/
+   
+    //var query 
+    return "funciona2";
+  }
+}
+
+export class eventService2 {
+  getEventDetail(id) 
+  {
+  /*En el detalle deberán listarse todos los atributos del Evento, como así también su localizacion
+  completa. Esto incluye la localidad y la provincia donde se encuentra la localización*/
+
+  //var query 
+  return "funciona3"
+  }
+}
+export class eventService3 {
+getListadoParticipantes(){}
 
 
-      /* const query = `select * from events limit ${pageSize} offset ${requestedPage}`;
+}
+/* const query = `select * from events limit ${pageSize} offset ${requestedPage}`;
         const query2 = `select count(*) from events`
         throw new Error("Error en el servicio");
 
@@ -30,5 +50,3 @@ export class eventService{
             total: query2,
             },
         };*/
-
-
